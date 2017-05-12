2970a8e2bcc69f350bcc41af03570493e997faf5
David Patterson <David Patterson>
2017-05-11 23:07:36 -0700

    section2

--- client/main.ts
+++ client/main.ts
@@ -25,9 +25,17 @@ import 'zone.js';
 import 'reflect-metadata';
  
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { MeteorObservable } from 'meteor-rxjs';
 import { Meteor } from 'meteor/meteor';
 import { AppModule } from './imports/app/app.module';
  
 Meteor.startup(() => {
-  platformBrowserDynamic().bootstrapModule(AppModule);
+  const subscription = MeteorObservable.autorun().subscribe(() => {
+    if (Meteor.loggingIn()) {
+      return;
+    }
+ 
+    setTimeout(() => subscription.unsubscribe());
+    platformBrowserDynamic().bootstrapModule(AppModule);
+  });
 });
